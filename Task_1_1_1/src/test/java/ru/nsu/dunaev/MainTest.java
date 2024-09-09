90007a5452842bec45dcafd55d60e0ab7c36081d
deudeuka <deudeukacs@gmail.com>
2024-09-10 00:14:53 +0700

    added main

--- Task_1_1_1/src/test/java/ru/nsu/dunaev/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/dunaev/MainTest.java
@@ -5,5 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
-
+    @Test
+    void mainTest(){
+        Main.main(new String[]{});
+        assertTrue(true);
+    }
 }
