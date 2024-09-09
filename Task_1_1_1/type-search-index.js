efe4407ef7c0aa507664090344d7ae22e4bfc438
DeuDeuka <DeuDeuka@users.noreply.github.com>
2024-09-09 17:15:55 +0000

    Deploying to gh-pages from @ DeuDeuka/OOP@42397d3fa1e4ad42ff4ddd36b6a1958dc3c98446 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dunaev","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.dunaev","l":"HeapSort"},{"p":"ru.nsu.dunaev","l":"Main"}]
