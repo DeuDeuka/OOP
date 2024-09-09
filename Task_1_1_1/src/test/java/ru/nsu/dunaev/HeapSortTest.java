451e08025fb3c8790ee47394fa511aa51133fd3c
deudeuka <deudeukacs@gmail.com>
2024-09-10 00:15:14 +0700

    added main

--- Task_1_1_1/src/test/java/ru/nsu/dunaev/HeapSortTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/dunaev/HeapSortTest.java
@@ -1,11 +1,8 @@
 package ru.nsu.dunaev;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Random;
 
 
 class HeapSortTest {
